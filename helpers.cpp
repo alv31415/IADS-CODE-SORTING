f5f683cbbf427cfb139391ccb1a8d2a0ecbe1e95
Antonio León Villares <aleonvillares31415@gmail.com>
2021-05-19 22:12:27 +0100

    Makefile added

---
+++ helpers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "helpers.h"
+
+using namespace std;
+
+string vectorToString (vector<int> array) {
+    string str = "[";
+    for (int n = 0; n < array.size(); n++) {
+
+        string element = to_string(array[n]);
+
+        if (n == array.size() - 1){
+            str += (element+ "]");
+        }
+        else {
+            str += (element+ ", ");
+        }
+    };
+
+    return str;
+};
--- insertSort.cpp
+++ insertSort.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include "helpers.h"
 
 using namespace std;
 
+vector<int> insertSort (vector<int> array) {
+    int n = array.size();
+    for (int i = 0; i < n; i++) {
+        int x = array[i];
+        int j = i - 1;
+        // until we find an element which is smaller than x, iterate
+        // switching x and the element to tis left
+        while (j >= 0 && array[j] > x) {
+            array[j+1] = array[j];
+            j -= 1;
+        }
+        // once an element smaller than x is found, we insert x in front of it
+        array[j+1] = x;
+
+    };
+
+    return array;
+
+};
+
 int main() {
-    cout << "Hello World";
-}
+    vector<int> testArray = {5,8,9,-1,2,4,64,2,7};
+    cout << "Vector given: "<< vectorToString(testArray) << "\n";
+    cout << "Vector sorted: "<< vectorToString(insertSort(testArray)) << "\n";
+};
